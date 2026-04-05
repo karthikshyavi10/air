a536b0eec3400674d3676595cc5ac4d40b75e768
karthikshyavi10 <karthikshyavi10e@gmail.com>
2026-04-05 18:34:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ async function predictAQI() {
     const pm10 = document.getElementById("pm10").value;
     const no2 = document.getElementById("no2").value;
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("http://192.168.1.9:5000/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
